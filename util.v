d5b121796a5b1428e360a00dadc64295796ca1bd
hassieswift621 <hey@hassie.ninja>
2020-04-20 17:30:06 +0100

    Add more utils

--- util.v
+++ util.v
@@ -1,11 +1,33 @@
 module dsnowflake
 
+import strconv
+
+// parse_epoch_ms parses an epoch milliseconds timestamp to a snowflake.
+pub fn parse_epoch_ms(epoch u64) &?Snowflake {
+	if epoch < discord_epoch {
+		return error('dsnowflake: epoch cannot be earlier than the Discord epoch')
+	}
+	return new((epoch - discord_epoch) << 22)
+}
+
+// parse_epoch_s parses an epoch seconds timestamp to a snowflake.
+pub fn parse_epoch_s(epoch u64) &?Snowflake {
+	return parse_epoch_ms(epoch * 1000)
+}
+
 // parse_i64 parses an i64 to a snowflake.
 pub fn parse_i64(id i64) &Snowflake {
-	return dsnowflake.new(u64(id))
+	return new(u64(id))
 }
 
 // parse_u64 parses a u64 to a snowflake.
 pub fn parse_u64(id u64) &Snowflake {
-	return dsnowflake.new(id)
+	return new(id)
+}
+
+// parse_string parses a string to a snowflake.
+pub fn parse_string(id string) &Snowflake {
+	// TODO: Return error once strconv returns errors.
+	i := strconv.parse_uint(id, 10, 64)
+	return new(i)
 }
--- v.mod
+++ v.mod
@@ -5,6 +5,7 @@ Module {
 	author: 'Hassie'
 	version: '0.1.0'
 	description: 'A V library for handling Discord snowflake IDs',
+	dependencies: [],
 	tags: ['discord', 'api', 'snowflake']
 	license: 'Apache-2.0',
 	repo_url: 'https://github.com/hassieswift621/discord-vflake',
